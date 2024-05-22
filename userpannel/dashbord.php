fcca8820905fc77d8c26b22929b7bd00cfa1eb37
shyam <shyam18061996@gmail.com>
2024-05-22 12:57:48 +0530

    22/5

--- userpannel/dashbord.php
+++ userpannel/dashbord.php
@@ -26,14 +26,16 @@
                                 </div>
 
                             </div>
-                          
-                            
+
+
                         </div>
-                        <div class="col-lg-12">
+
+                        <div class="EarningSection">
+                        <div class="row">
+                            <div class="col-lg-12">
                                 <h2 class='headigEarn'>Earnings</h2>
                             </div>
-                        <div class="row">
-                        <div class="col-lg-3 col-6">
+                            <div class="col-lg-3 col-6">
                                 <div class="card border-0 cardbg"
                                     style='box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px; border-radius: 16px;'>
                                     <img src="images/whiterupisym.png" alt="">
@@ -74,33 +76,34 @@
                                 </div>
                             </div>
                         </div>
+                        </div>
 
                     </div>
                 </div>
                 <div class="col-lg-5 col-12">
                     <div class="dashRight bg-white">
                         <h2 class='mb-4'>Recent sales</h2>
-                        <div class="totalrecent mb-4">
+                        <div class="totalrecent ">
                             <img src="images/main.png" alt="img">
                             <p>PRITAM BHOYAR<br>0000-00-00 00:00:00</p>
                             <h3>₹ 1400</h3>
                         </div>
-                        <div class="totalrecent mb-4">
+                        <div class="totalrecent">
                             <img src="images/main.png" alt="img">
                             <p>PRITAM BHOYAR<br>0000-00-00 00:00:00</p>
                             <h3>₹ 1400</h3>
                         </div>
-                        <div class="totalrecent mb-4">
+                        <div class="totalrecent">
                             <img src="images/main.png" alt="img">
                             <p>PRITAM BHOYAR<br>0000-00-00 00:00:00</p>
                             <h3>₹ 1400</h3>
                         </div>
-                        <div class="totalrecent mb-4">
+                        <div class="totalrecent">
                             <img src="images/main.png" alt="img">
                             <p>PRITAM BHOYAR<br>0000-00-00 00:00:00</p>
                             <h3>₹ 1400</h3>
                         </div>
-                        <div class="totalrecent mb-4">
+                        <div class="totalrecent">
                             <img src="images/main.png" alt="img">
                             <p>PRITAM BHOYAR<br>0000-00-00 00:00:00</p>
                             <h3>₹ 1400</h3>
@@ -109,7 +112,7 @@
                     </div>
                 </div>
             </div>
-            <div class="row">
+            <div class="row d-none">
                 <div class="col-lg-4">
                     <div class="leftGraphimg">
                         <img src="images/graph1.png" alt="img" class='w-100'>
